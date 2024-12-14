57c6d2b3d6e670b792c24396a8cf9c162b984001
Tim C <knb143@my.utsa.edu>
2024-12-14 04:57:35 +0000

    lab completed

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -32,6 +32,10 @@ public class Lab {
      * @return a string representation of all values of nums, with the values separated by spaces.
      */
     public String arrayToString(int[] nums){
-        return "";
+        String result = "";
+        for(int i : nums){
+            result += i + " ";
+        }
+        return result;
     }
 }
